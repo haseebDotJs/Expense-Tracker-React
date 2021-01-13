6870115d62fcfa358ef670b2eec185e80cfa0bef
haseebDotJs <haseeb.khalid03452800681@gmail.com>
2021-01-13 12:24:51 +0500

    Change the name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div>
-            <h2>Expense Tracker by Abdul Haseeb</h2>
+            <h2>Expense Tracker by Haseeb Ahmad</h2>
         </div>
     )
 }
